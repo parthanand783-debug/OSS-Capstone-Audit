91218de2a591eef5612406d3880b4e23277d1afb
parthanand783-debug <parthanand783@gmail.com>
2026-03-29 23:19:57 +0530

    Update script5.sh

--- script5.sh
+++ script5.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # Script 5: Open Source Manifesto Generator
-# Author: Aryawart Gupta
+# Author: Parth Anand
 # Course: Open Source Software
 
 echo "========================================="
